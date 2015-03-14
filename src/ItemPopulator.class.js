e11a813de7af1ea8d4602c389f17b1f1ae2920b1
slashman <java.koder@gmail.com>
2015-03-13 20:57:07 -0500

    Add items

--- src/ItemPopulator.class.js
+++ src/ItemPopulator.class.js
@@ -6,6 +6,7 @@ var Util = require('./Utils');
 
 ItemPopulator.prototype = {
 	populateLevel: function(sketch, level){
+		this.calculateRarities(level.depth);
 		for (var i = 0; i < sketch.areas.length; i++){
 			var area = sketch.areas[i];
 			this.populateArea(area, level);
@@ -15,13 +16,66 @@ ItemPopulator.prototype = {
 		var items = Util.rand(0,2);
 		for (var i = 0; i < items; i++){
 			var position = level.getFreePlace(area);
-			var item = this.getAnItem(level.depth);
+			var item = this.getAnItem();
 			level.addItem(item, position.x, position.y);
 		}
 	},
-	ITEMS: ['dagger'],
-	getAnItem: function(depth){
-		return Util.randomElementOf(this.ITEMS);
+	calculateRarities: function(depth){
+		this.thresholds = [];
+		this.generationChanceTotal = 0;
+		for (var i = 0; i < this.ITEMS.length; i++){
+			var item = this.ITEMS[i];
+			var malus = Math.abs(depth-item.depth) > 1;
+			var rarity = malus ? item.rarity / 2 : item.rarity;
+			this.generationChanceTotal += rarity;
+			this.thresholds.push({threshold: this.generationChanceTotal, item: item});
+		}
+	},
+	ITEMS: [
+		/*{code: 'dagger', rarity: 3500},
+		{code: 'oilFlask', rarity: 1400},
+		{code: 'staff', rarity: 350},
+		{code: 'sling', rarity: 280},
+		{code: 'mace', rarity: 70},
+		{code: 'axe', rarity: 31},
+		{code: 'bow', rarity: 28},
+		{code: 'sword', rarity: 350},
+		{code: 'halberd', rarity: 23},
+		{code: 'crossbow', rarity: 11},
+		{code: 'magicAxe', rarity: 5},
+		{code: 'magicBow', rarity: 4},
+		{code: 'magicSword', rarity: 4},
+		{code: 'magicWand', rarity: 2},
+		{code: 'cloth', rarity: 140},
+		{code: 'leather', rarity: 35},
+		{code: 'chain', rarity: 12},
+		{code: 'plate', rarity: 4},
+		{code: 'magicChain', rarity: 2},
+		{code: 'magicPlate', rarity: 1}*/
+		{code: 'cure', rarity: 1000, depth: 1},
+		{code: 'heal', rarity: 1000, depth: 1},
+		{code: 'redPotion', rarity: 1000, depth: 1},
+		{code: 'yellowPotion', rarity: 1000, depth: 1},
+		{code: 'light', rarity: 1000, depth: 2},
+		{code: 'missile', rarity: 1000, depth: 3},
+		{code: 'iceball', rarity: 500, depth: 4},
+		{code: 'repel', rarity: 500, depth: 5},
+		{code: 'blink', rarity: 333, depth: 5},
+		{code: 'fireball', rarity: 333, depth: 6},
+		{code: 'protection', rarity: 250, depth: 6},
+		{code: 'time', rarity: 200, depth: 7},
+		{code: 'sleep', rarity: 200, depth: 7},
+		{code: 'jinx', rarity: 166, depth: 8},
+		{code: 'tremor', rarity: 166, depth: 8},
+		{code: 'kill', rarity: 142, depth: 8}
+	],
+	getAnItem: function(){
+		var number = Util.rand(0, this.generationChanceTotal);
+		for (var i = 0; i < this.thresholds.length; i++){
+			if (number <= this.thresholds[i].threshold)
+				return this.thresholds[i].item.code;
+		}
+		return this.thresholds[0].item.code;
 	}
 }
 
